11df477135378aebe2e1b5c5ea01cd64e1357f5f
Nathaniel Gauvin <nategauvin@gmail.com>
2025-04-03 11:39:38 -0400

    Update testDataFunctions.R

--- functions/testDataFunctions.R
+++ functions/testDataFunctions.R
@@ -172,7 +172,7 @@ add_survival_data_GSE6891 <- function(dataset) {
 # Connecting to MongoDB
 connect_mongo <- function(collection_name, user = "root", pass = "password", host = "localhost:27017") {
   uri <- sprintf("mongodb://%s:%s@%s/", user, pass, host)
-  return (mongo(url = uri, db = "database", collection = collection_name))
+  return (mongo(url = uri, db = "aml-bet", collection = collection_name))
   
 }
 
