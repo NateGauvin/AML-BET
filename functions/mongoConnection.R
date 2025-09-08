0bcee6b4111d283f491bbfa6a0f21850467f9fcc
Nathaniel Gauvin <nategauvin@gmail.com>
2025-09-08 16:36:35 -0400

    Fixed mongo connection

--- functions/mongoConnection.R
+++ functions/mongoConnection.R
@@ -1,13 +1,13 @@
 library(mongolite)
 
 # Connecting to MongoDB
-#connect_mongo <- function(collection_name, user = "root", pass = "password", host = "localhost:27017") {
-#  uri <- sprintf("mongodb://%s:%s@%s/", user, pass, host)
-#  return (mongo(url = uri, db = "aml-bet", collection = collection_name))
-#  
-#}
+connect_mongo <- function(collection_name, user = "root", pass = "password", host = "localhost:27017") {
+ uri <- sprintf("mongodb://%s:%s@%s/", user, pass, host)
+ return (mongo(url = uri, db = "aml-bet", collection = collection_name))
+
+}
 
 # Overloaded method for RStudio use
-connect_mongo <- function(collection_name) {
-  return(mongo(collection = collection_name, db = "aml-bet"))
-}
+# connect_mongo <- function(collection_name) {
+#   return(mongo(collection = collection_name, db = "aml-bet"))
+# }
